c86605d470533c5cd8c1da244abe4907228f83d1
al-jpacho <jordanapacho@gmail.com>
2025-06-13 21:45:41 +0100

    add query to get count of tickets by registration_state

---
+++ nyc_parking_violations/models/gold/gold_vehicles_metrics.sql
@@ -0,0 +1,6 @@
+select registration_state
+  , count(summons_number) as ticket_count
+from {{ref('silver_violation_vehicles')}}
+where registration_state != 'NY'
+group by  registration_state
+order by ticket_count desc
