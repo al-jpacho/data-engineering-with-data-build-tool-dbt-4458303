79ba1e60def4a4e6a9335182ef713efd1e94c966
al-jpacho <jordanapacho@gmail.com>
2025-06-13 21:47:25 +0100

    add query to get ticket count and revenue sum grouped by violation code

--- nyc_parking_violations/models/gold/gold_ticket_metrics.sql
+++ nyc_parking_violations/models/gold/gold_ticket_metrics.sql
@@ -1,6 +1,6 @@
-select registration_state
-  , count(summons_number) as ticket_count
-from {{ref('silver_violation_vehicles')}}
-where registration_state != 'NY'
-group by  registration_state
-order by ticket_count desc
+select violation_code,
+  ,  count(summons_number) as ticket_count
+  ,  SUM(fee_usd) as total_revenue_usd
+from {{ref('silver_violation_tickets')}}
+group by violation_code
+order by total_revenue_usd desc
